d356ce6db828cec3f3561888f5a0c0a774d0187d
GilchrisK <113191313+GilchrisK@users.noreply.github.com>
2023-06-30 16:04:20 +0100

    Update project_tkinterLogin.py

--- Computer Project Folder/PROJECT - Projectiles/project_tkinterLogin.py
+++ Computer Project Folder/PROJECT - Projectiles/project_tkinterLogin.py
@@ -5,12 +5,59 @@ from tkinter import ttk #module that contains several different widgets such as 
 
 
 
-def adminPage():
-    adminWindow = Tk()
-    starter_Window.destroy()
+def back():
+    adminPage.destroy()
+    starter_Window()
 
 def switch_signUp():
-    notebook.select(tab_id=tab2)
+    notebook.select(tab_id=signUp_tab)
+
+def adminPage():
+    adminWindow = Tk()
+    adminWindow.geometry("500x400")
+    starter_Window.destroy()
+
+    # adminLogin_lbl = Label(adminWindow,text="Login")
+    # adminLogin_lbl.pack(side=LEFT)
+    #
+    # adminEntry_Username = Entry(adminWindow,
+    #                             font=("Comic sans", 35),
+    #                             show="*",
+    #                            )
+    #
+    # adminEntry_Username.pack()
+    #
+    # adminPassword_lbl = Label(adminWindow, text="Password")
+    # adminPassword_lbl.pack(side=LEFT)
+    #
+    # adminEntry_Password = Entry(adminWindow,
+    #                             font=("Comic sans", 35),
+    #                             show="*",
+    #                             )
+
+    titleLabel = Label(adminWindow, text=" Please enter your login details... ")
+    titleLabel.grid(row=0, column=0, columnspan=3, sticky="SNEW", pady=10, padx=10)
+
+    adminLogin_lbl = Label(adminWindow, text="Username")
+    adminLogin_lbl.grid(row=1, column=0, padx=10, pady=10, sticky="W")
+
+    adminEntry_Username = Entry(adminWindow, width="30")
+    adminEntry_Username.grid(row=1, column=1, padx=10, pady=10, sticky="E")
+
+    adminPassword_lbl = Label(adminWindow, text="Password")
+    adminPassword_lbl.grid(row=2, column=0, padx=10, pady=10, sticky="W")
+
+    adminEntry_Password = Entry(adminWindow, width="30")
+    adminEntry_Password.grid(row=2, column=1, padx=10, pady=10, sticky="E")
+
+    backButton = Button(adminWindow, text="Back", command=lambda: back(adminWindow))
+    backButton.grid(row=4, column=0, sticky="SNEW", padx=10, pady=10)
+
+
+
+    # adminEntry_Password.pack()
+
+
 
 
 starter_Window = Tk()
@@ -26,47 +73,35 @@ notebook = ttk.Notebook(starter_Window) #A widget that manages a collection of w
 # frame.place(x=100,y=0)
 
 
-tab1 = Frame(notebook,padx=100)  #This will be a a new frame for tab1
-tab2 = Frame(notebook,padx=100)  #New frame for tab2
+login_tab = Frame(notebook, padx=100)  #This will be a a new frame for tab1
+signUp_tab = Frame(notebook, padx=100)  #New frame for tab2
 
-notebook.add(tab1,text="Log in")
-notebook.add(tab2,text="Sign Up")
+notebook.add(login_tab, text="Log in")
+notebook.add(signUp_tab, text="Sign Up")
 notebook.pack()  #This will expand to fill any space
 
 # notebook.config(height=notebook.size())               #fill= will fill space on the x and y-axis
 
-label1 = Label(tab1,text="Welcome back!",font=("Comic Sans",20),width=15,height=2,anchor='center',bg="light grey")
-label1.pack(pady=60)
-label3 = Label(tab1,text="Choose the account to login with :)",font=("Comic sans",20),bg="light grey")
-label3.place(x=30,y=160)
+login_lbl1 = Label(login_tab, text="Welcome back!", font=("Comic Sans", 20), width=15, height=2, anchor='center', bg="light grey")
+login_lbl1.pack(pady=60)
+login_lbl2 = Label(login_tab, text="Choose the account to login with :)", font=("Comic sans", 20), bg="light grey")
+login_lbl2.place(x=30, y=160)
 
-label2 = Label(tab2,text="This is tab2",width=70,height=60)
-label2.pack()
+signUp_lbl1 = Label(signUp_tab, text="This is tab2", width=70, height=60)
+signUp_lbl1.pack()
 
-admin_btn = Button(tab1,text="Admin",width=20,height=5,command=adminPage,bg="light grey")
+admin_btn = Button(login_tab, text="Admin", width=20, height=5, command=adminPage, bg="light grey")
 admin_btn.place(x=100,y=300)
 
-student_btn = Button(tab1,text="Student",width=20,height=5,command=adminPage,bg="light grey")
+student_btn = Button(login_tab, text="Student", width=20, height=5, command=adminPage, bg="light grey")
 student_btn.place(x=250,y=300)
 
-signUp_btn = Button(tab1,text="Sign Up",width=10,height=1,command=switch_signUp)
+signUp_btn = Button(login_tab, text="Sign Up", width=10, height=1, command=switch_signUp)
 signUp_btn.place(x=330,y=420)
 
-label4 = Label(tab1,text="Dont have an account? Click here...",font=("Comic Sans",11))
-label4.place(x=80,y=425)
-
-
-
-starter_Window.mainloop()
-
-
-
-
-
-
-
-
-
+login_lbl3 = Label(login_tab, text="Dont have an account? Click here...", font=("Comic Sans", 11))
+login_lbl3.place(x=80,y=425)
+###########################
 
 
 
@@ -76,6 +111,24 @@ starter_Window.mainloop()
 
 
 starter_Window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
