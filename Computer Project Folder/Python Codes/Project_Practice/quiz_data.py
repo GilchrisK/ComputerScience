5f012738dcb290e23317296d73698add50eaf070
GilchrisK <113191313+GilchrisK@users.noreply.github.com>
2024-02-29 12:49:52 +0000

    Add files via upload

---
+++ Computer Project Folder/Python Codes/Project_Practice/quiz_data.py
@@ -0,0 +1,23 @@
+quiz_data = [
+    {
+        "question": "Name the vector quantity?",
+        "choices": ["Speed", "Mass", "Temperature", "Displacement"],
+        "answer": "Displacement"
+    },
+    {
+        "question": "What is the product of speed and time?",
+        "choices": ["Displacement", "Distance", "Acceleration", "Velocity"],
+        "answer": "Distance"
+    },
+    {
+        "question": "What is the closest value of gravity on Earth?",
+        "choices": ["10N", "9.8N", "15N", "11N"],
+        "answer": "9.8N"
+    },
+    {
+        "question": "If Adam runs 10 metres in 5 seconds then what is his speed?",
+        "choices": ["2 m/s", "5 m/s", "0.5 m/s", "50 m/s"],
+        "answer": "2 m/s"
+    }
+    # Add more questions here
+]
