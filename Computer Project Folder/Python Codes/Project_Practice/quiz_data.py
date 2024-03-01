bae00df39bd86d4c8c9f24e414fa26ab843f91d3
GilchrisK <113191313+GilchrisK@users.noreply.github.com>
2024-03-01 16:09:59 +0000

    Update quiz_data.py
    Newest

--- Computer Project Folder/Python Codes/Project_Practice/quiz_data.py
+++ Computer Project Folder/Python Codes/Project_Practice/quiz_data.py
@@ -18,6 +18,11 @@ quiz_data = [
         "question": "If Adam runs 10 metres in 5 seconds then what is his speed?",
         "choices": ["2 m/s", "5 m/s", "0.5 m/s", "50 m/s"],
         "answer": "2 m/s"
+    },
+    {
+        "question": "What is the only force acting on an arrow projectile after it is launched is..?",
+        "choices": ["Push force", "Gravity force", "Launch force", "Electric force"],
+        "answer": "Distance"
     }
     # Add more questions here
 ]
