199776dc33e3bad21c6687b1461979c13a4557ee
GilchrisK <113191313+GilchrisK@users.noreply.github.com>
2024-03-21 12:44:41 +0000

    Update projectiles_code.py
    Rn

--- Computer Project Folder/Python Codes/Project_Practice/projectiles_code.py
+++ Computer Project Folder/Python Codes/Project_Practice/projectiles_code.py
@@ -29,27 +29,40 @@ def main() -> None:
     shoot = False
     angle = 0
     speed = 100
-    graph_icon: pygame.Surface
+    graph_icon2: pygame.Surface
+    moving = False
+
+
+
+
+
 
 
 
     screen = projectile_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Angle: 0 Speed: 200")
-    ground_surface = pygame.image.load("grass_surface.png").convert_alpha()
+    bigHorizontal_ruler_surface = pygame.image.load("x_ruler3.png").convert_alpha()
     ground_x = 0
     ground_y = screen.get_height() - 150
+    bigHorizontal_ruler = bigHorizontal_ruler_surface.get_rect(midbottom=(ground_x + 600, ground_y + 21))
+    ground_surface = pygame.image.load("grass_surface.png").convert_alpha()
     graph_x = screen.get_height() + 300
     graph_y = screen.get_height() - 780
+    # xruler_x = screen.get_height() + 300
+    # xruler_y = screen.get_height() - 690
     background = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
     background.fill((222, 237, 244))
     projectile = pygame.image.load("cannon ball.png")
     projectile = projectile.convert_alpha()
     flag = pygame.image.load("flag.png")
     flag = flag.convert_alpha()
-    graph_icon = pygame.image.load("graph_icon.png")
-    graph_icon = graph_icon.convert_alpha()
+    graph_icon2 = pygame.image.load("graph_icon2.png")
+    graph_icon2 = graph_icon2.convert_alpha()
+    horizontal_ruler_surface = pygame.image.load("x_ruler1.png").convert_alpha()
+    horizontal_ruler = horizontal_ruler_surface.get_rect(midbottom = (screen.get_height() + 325,screen.get_height() - 690))
     start_y = screen.get_height() - ground_surface.get_height() - projectile.get_height()
     y = start_y
     clock: pygame.time.Clock = pygame.time.Clock()
+
 
 
 
@@ -73,6 +86,29 @@ def main() -> None:
                     speed -= 10
                 elif e.__dict__["key"] == pygame.K_SPACE:
                     shoot = True
+
+        mouse_position = pygame.mouse.get_pos()
+        if horizontal_ruler.collidepoint(mouse_position):
+
+            if e.type == pygame.MOUSEBUTTONDOWN:  # Prints output when you hold the mouse down
+                background.blit((bigHorizontal_ruler_surface), (bigHorizontal_ruler))
+
+                print("yes")
+        #         for e in pygame.event.get():
+        #             if e.tye == pygame.MOUSEBUTTONUP:
+        #                 moving = False
+        #             elif e.type == pygame.MOUSEBUTTONDOWN:
+        #                 bigHorizontal_ruler.collidepoint(e.pos)
+        #                 moving = True
+        #             elif e.type == pygame.MOUSEMOTION and moving:
+        #                 bigHorizontal_ruler.move_ip(e.rel)
+        # pygame.display.update()
+
+
+
+
+
+
 
 
 
@@ -103,7 +139,8 @@ def main() -> None:
         screen.blit(background, (0, 0))
         screen.blit(projectile, (x, y))
         screen.blit(ground_surface,(ground_x,ground_y))
-        screen.blit(graph_icon,(graph_x, graph_y))
+        screen.blit(graph_icon2,(graph_x, graph_y))
+        screen.blit(horizontal_ruler_surface,(horizontal_ruler))
         # pygame.draw.circle(screen,"#000000", (int(x), int(y)), 5)
         pygame.display.flip()
 
